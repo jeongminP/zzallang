b5b880289d6c3edf30a66a291557b80f720de783
jeongminP <pjm9355@naver.com>
2020-01-23 17:15:02 +0900

    공유지출 추가뷰 바디를 변수로 쪼개기

--- zzallang/UI/SharedNewExpenditureView.swift
+++ zzallang/UI/SharedNewExpenditureView.swift
@@ -21,6 +21,76 @@ struct SharedNewExpenditureView: View {
     @State private var time = Date()
     @State private var showingAlert = false
     
+    var paymentPicker: some View {
+        Picker("Payment", selection: $newItem.payment) {
+            ForEach(Payment.allCases, id: \.self) {
+                Text($0.toKoreanString()).tag($0)
+            }
+        }
+        .pickerStyle(SegmentedPickerStyle())
+    }
+    
+    var titleField: some View {
+        HStack {
+            Text("지출명").font(.headline)
+                .padding()
+            TextField("내역 이름", text: $newItem.title)
+        }
+    }
+    
+    var priceField: some View {
+        HStack {
+            Button(action: { self.flipCurrency() }) {
+                Text(newItem.currency.toString())
+                    .padding()
+            }
+            TextField("0", text: $priceString)
+        }
+    }
+    
+    var payerPicker: some View {
+        HStack {
+            Text("지출한 사람").font(.headline)
+                .padding()
+            Picker("Payer", selection: $newItem.payer) {
+                ForEach(tripData.personalList, id: \.self) {
+                    Text($0.userId).tag($0.userId)
+                }
+            }
+            .pickerStyle(SegmentedPickerStyle())
+        }
+    }
+    
+    var categoryPicker: some View {
+        HStack {
+            Picker("Category", selection: $newItem.category) {
+                ForEach(Category.allCases, id: \.self) {
+                    $0.image().tag($0)
+                }
+            }
+            .pickerStyle(SegmentedPickerStyle())
+            .padding()
+        }
+    }
+    
+    var timePicker: some View {
+        HStack {
+            Text("시간").font(.headline)
+                .padding()
+            Spacer()
+            DatePicker("time", selection: $time, displayedComponents: .hourAndMinute)
+                .labelsHidden()
+        }
+    }
+    
+    var memoField: some View {
+        HStack {
+            Text("메모").font(.headline)
+                .padding()
+            TextField("메모", text: $newItem.memo)
+        }
+    }
+    
     var makingNewExpenditureButton: some View {
         Button(action: { self.verifyToMakeNewExpenditure() }) {
             Text("확인")
@@ -33,59 +103,17 @@ struct SharedNewExpenditureView: View {
     
     var body: some View {
         VStack(alignment: HorizontalAlignment.leading) {
-            Picker("Payment", selection: $newItem.payment) {
-                ForEach(Payment.allCases, id: \.self) {
-                    Text($0.toKoreanString()).tag($0)
-                }
-            }
-            .pickerStyle(SegmentedPickerStyle())
-            
-            HStack {
-                Text("지출명").font(.headline)
-                    .padding()
-                TextField("내역 이름", text: $newItem.title)
-            }
-            HStack {
-                Button(action: { self.flipCurrency() }) {
-                    Text(newItem.currency.toString())
-                        .padding()
-                }
-                TextField("0", text: $priceString)
-            }
-            HStack {
-                Text("지출한 사람").font(.headline)
-                    .padding()
-                Picker("Payer", selection: $newItem.payer) {
-                    ForEach(tripData.personalList, id: \.self) {
-                        Text($0.userId).tag($0.userId)
-                    }
-                }
-                .pickerStyle(SegmentedPickerStyle())
-            }
+            paymentPicker
+            titleField
+            priceField
+            payerPicker
             
             // TODO: 관련된 사람 복수선택 버튼 추가
             
-            HStack {
-                Picker("Category", selection: $newItem.category) {
-                    ForEach(Category.allCases, id: \.self) {
-                        $0.image().tag($0)
-                    }
-                }
-                .pickerStyle(SegmentedPickerStyle())
-                .padding()
-            }
-            HStack {
-                Text("시간").font(.headline)
-                    .padding()
-                Spacer()
-                DatePicker("time", selection: $time, displayedComponents: .hourAndMinute)
-                    .labelsHidden()
-            }
-            HStack {
-                Text("메모").font(.headline)
-                    .padding()
-                TextField("메모", text: $newItem.memo)
-            }
+            categoryPicker
+            timePicker
+            memoField
+            
             makingNewExpenditureButton
         }
         .navigationBarTitle("공유지출 추가")
